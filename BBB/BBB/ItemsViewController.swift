cdea7a4c8cf3cb330653f0b0dee264baebc25d7a
Lucia Carrera <luciaacarreraa@gmail.com>
2021-11-08 22:50:41 -0500

    Revert "Merge branch 'Jade'"
    This reverts commit b50e558d11d56709c163e3b28993d7e893ee65cd, reversing
    changes made to e74bc7ada866602b156b04bd259739d90c2fc51a.

--- BBB/BBB/ItemsViewController.swift
+++ BBB/BBB/ItemsViewController.swift
@@ -92,18 +92,16 @@ class ItemsViewController: UITableViewController {
         // If the triggered segue is the showItem segue
         switch segue.identifier {
         case "showItem":
-            // Figure out which row was just tapped
             if let row = tableView.indexPathForSelectedRow?.row {
-                // Get the item associated with this row and pass it along
                 let item = itemStore.allItems[row]
                 let detailViewController = segue.destination as! DetailViewController
                 detailViewController.item = item
             }
             
-        default:
-            preconditionFailure("Unexpected segue identifier.")
+        default: preconditionFailure("Unexpected segue identifier.")
             
         }
+        
     }
 }
 
