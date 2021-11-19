8db621fe11c0c2504c885209549911f4867db844
Lucía Carrera <61793180+luciacarrera@users.noreply.github.com>
2021-11-19 16:12:52 -0500

    ch4 done

---
+++ ios_task02/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Lucia Carrera Saenz on 11/18/21.
+//  Copyright © 2021 Lucia Carrera Saenz. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+
+}
+
---
+++ ios_task02/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Lucia Carrera Saenz on 11/19/21.
+//  Copyright © 2021 Lucia Carrera Saenz. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
