3da629108fa13a41e566863bc36f0651d27e1cf8
jaderigout1 <jrigout@uvm.edu>
2021-11-29 00:11:31 -0500

    finished chapter 5 + bonze challenge

--- ios_task02/WorldTrotter/WorldTrotter/MapViewController.swift
+++ ios_task02/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,8 +7,91 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+    
+    @objc func poiDisplay(_ poiSwitch: UISwitch) {
+        if poiSwitch.isOn {
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [])
+        } else {
+            mapView.pointOfInterestFilter = MKPointOfInterestFilter(including: [])
+        }
+    }
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        //Set it as 'the' view of this view controller
+        view = mapView
+        
+        // Segment control
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        // Points of intrest label
+        let pointsOfInterest = UILabel()
+        pointsOfInterest.text = "Points of Interest"
+        pointsOfInterest.textColor = UIColor.black
+        
+        pointsOfInterest.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pointsOfInterest)
+        
+        let pointsLabelTopConstraint = pointsOfInterest.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8)
+        let pointsLabelLeadingConstraint = pointsOfInterest.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        
+        pointsLabelTopConstraint.isActive = true
+        pointsLabelLeadingConstraint.isActive = true
+        
+        // Points of interest switch
+        let ptsOfIntSwitch = UISwitch()
+        ptsOfIntSwitch.isOn = true
+        
+        ptsOfIntSwitch.addTarget(self, action: #selector(poiDisplay(_ :)), for: .valueChanged)
+        
+        ptsOfIntSwitch.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(ptsOfIntSwitch)
+        
+        let switchTopConstraint = ptsOfIntSwitch.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8)
+        let switchLeadingConstraint = ptsOfIntSwitch.leadingAnchor.constraint(equalTo: pointsOfInterest.trailingAnchor, constant: 12)
+        
+        switchTopConstraint.isActive = true
+        switchLeadingConstraint.isActive = true
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
